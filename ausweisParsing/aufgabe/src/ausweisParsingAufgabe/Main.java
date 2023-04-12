8e8c79045db1afde87c91dd2156c474a6697d5ca
Voreck Lukas <voreck-lukas@tutanota.com>
2023-04-12 20:10:11 +0200

    Fix ausweis input

--- ausweisParsing/aufgabe/src/ausweisParsingAufgabe/Main.java
+++ ausweisParsing/aufgabe/src/ausweisParsingAufgabe/Main.java
@@ -1,7 +1,14 @@
 package ausweisParsingAufgabe;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String line1 = scanner.nextLine();
+		String line2 = scanner.nextLine();
+		String line3 = scanner.nextLine();
+
 		String ausweisnummer = "";
 		String geburtsdatum = "";
 		String gültigkeitsdatum = "";
