dd9329c8c37a120ba7fe901c0d65c0b1a1be3606
Voreck Lukas <voreck-lukas@tutanota.com>
2023-04-12 17:59:08 +0200

    Date Parsing Aufgabe

---
+++ dateParsing/aufgabe/src/dateParsing/Main.java
@@ -0,0 +1,18 @@
+package dateParsing;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		
+		String tag = "";
+		String monat = "";
+		String jahr = "";
+		
+		System.out.println("Tag: " + tag);
+		System.out.println("Monat: " + monat);
+		System.out.println("Jahr: " + jahr);
+	}
+}
