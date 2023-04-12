929ddeab4ba9e01b345c33f2ae85dbf3228ee5a8
Voreck Lukas <voreck-lukas@tutanota.com>
2023-04-12 19:51:58 +0200

    Ausweis Parsen Aufgabe

---
+++ ausweisParsing/aufgabe/src/ausweisParsingAufgabe/Main.java
@@ -0,0 +1,13 @@
+package ausweisParsingAufgabe;
+
+public class Main {
+	public static void main(String[] args) {
+		String ausweisnummer = "";
+		String geburtsdatum = "";
+		String gültigkeitsdatum = "";
+		String versionsnummer = "";
+		String prüfziffer = "";
+		String familienName = "";
+		String vorname = "";
+	}
+}
